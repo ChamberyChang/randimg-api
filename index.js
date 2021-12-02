c7026fa14a7d4024dcbe9df65567e4277ec44266
ChamberyChang <sea_exp@163.com>
2021-12-02 22:33:12 +0900

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { existsSync, renameSync } = require('fs-extra');
+const { resolve } = require('path');
+const {
+  jsonc: { readSync: readJsoncSync },
+} = require('jsonc');
+
+try {
+  const CONFIG_PATH = resolve(__dirname, './config.jsonc');
+  const OLD_CONFIG_PATH = resolve(__dirname, './config.json');
+  const DEFAULT_CONFIG_PATH = resolve(__dirname, './config.default.jsonc');
+  // 配置迁移
+  if (existsSync(OLD_CONFIG_PATH) && !existsSync(CONFIG_PATH)) {
+    renameSync(OLD_CONFIG_PATH, CONFIG_PATH);
+  }
+  // 配置检查
+  readJsoncSync(CONFIG_PATH);
+  readJsoncSync(DEFAULT_CONFIG_PATH);
+} catch (e) {
+  const { code, message } = e;
+  const EOL = process.env.npm_execpath ? '\n' : '';
+  if (code === 'ENOENT') {
+    console.error(`ERROR: Configuration JSON not found ${e.path}${EOL}`);
+  } else if (message && message.includes('JSON')) {
+    console.error(`ERROR: Configuration JSON is format error\n${message}${EOL}`);
+  } else console.error(e);
+  process.exit(1);
+}
+
+process.on('SIGHUP', () => process.exit(128 + 1));
+process.on('SIGINT', () => process.exit(128 + 2));
+process.on('SIGTERM', () => process.exit(128 + 15));
+
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module);
+module.exports = require('./sendImg');
---
+++ sendImg.js
@@ -0,0 +1,135 @@
+import _, { random } from 'lodash';
+import { getLocalReverseProxyURL } from './pximg';
+import { URL } from 'url';
+import './utils/jimp.plugin';
+import Jimp from 'jimp';
+const Axios = require('./axiosProxy');
+
+const API_URL = 'https://api.lolicon.app/setu/v2'; //A public pixiv image dataset api
+
+const PIXIV_404 = Symbol('Pixiv image 404'); //when 404 is returned
+
+async function imgAntiShielding(url) {
+  const setting = global.config.setu;
+  const proxy = setting.pximgProxy.trim();
+  const img = await Jimp.read(
+    proxy ? Buffer.from(await Axios.get(url, { responseType: 'arraybuffer' }).then(r => r.data)) : url
+  );
+
+  switch (Number(global.config.setu.antiShielding)) {
+    case 1:
+      const [w, h] = [img.getWidth(), img.getHeight()];
+      const pixels = [
+        [0, 0],
+        [w - 1, 0],
+        [0, h - 1],
+        [w - 1, h - 1],
+      ];
+      for (const [x, y] of pixels) {
+        img.setPixelColor(Jimp.rgbaToInt(random(255), random(255), random(255), 1), x, y);
+      }
+      break;
+
+    case 2:
+      img.simpleRotate(90);
+      break;
+  }
+
+  return (await img.getBase64Async(Jimp.AUTO)).split(',')[1];
+}
+
+// the size of image is limited to 4M 
+function checkBase64RealSize(base64) {
+  return base64.length && base64.length * 0.75 < 4000000;
+}
+
+async function getAntiShieldingBase64(url, fallbackUrl) {
+  try {
+    const origBase64 = await imgAntiShielding(url);
+    if (checkBase64RealSize(origBase64)) return origBase64;
+  } catch (error) {
+    // the original image is too big
+  }
+  if (!fallbackUrl) return;
+  const m1200Base64 = await imgAntiShielding(fallbackUrl);
+  if (checkBase64RealSize(m1200Base64)) return m1200Base64;
+}
+
+function sendImg(context, at = true) {
+  const setuRegExec = context.message;
+  if (!setuRegExec) return false;
+
+  const setting = global.config.setu;
+  const proxy = setting.pximgProxy.trim();
+  const isGroupMsg = context.message_type === 'group';
+
+  const regGroup = setuRegExec.groups || {};
+  const r18 = regGroup.r18 && !(isGroupMsg && setting.r18OnlyInWhite && !setting.whiteGroup.includes(context.group_id));
+  const keyword = regGroup.keyword ? regGroup.keyword.split('&') : undefined;
+
+  let success = false;
+  Axios.post(API_URL, { r18, tag: keyword, size: ['original', 'regular'], proxy: null })
+    .then(ret => ret.data)
+    .then(async ret => {
+      if (ret.error) return global.replyMsg(context, ret.error, at);
+      if (!ret.data.length) return global.replyMsg(context, 'No results', at);
+
+      const setu = ret.data[0];
+      const setuUrl = setting.size1200 ? setu.urls.regular : setu.urls.original;
+      const urlMsgs = [`https://pixiv.net/i/${setu.pid} (p${setu.p})`];
+
+      if (
+        r18 &&
+        setting.r18OnlyUrl[
+          context.message_type === 'private' && context.sub_type !== 'friend' ? 'temp' : context.message_type
+        ]
+      ) {
+        global.replyMsg(context, urlMsgs.join('\n'), false, at);
+        return;
+      }
+      global.replyMsg(context, urlMsgs.join('\n'), at);
+
+      const getReqUrl = url => (proxy ? getSetuUrlByTemplate(proxy, setu, url) : getLocalReverseProxyURL(url));
+      const url = getReqUrl(setuUrl);
+      const fallbackUrl = setting.size1200 ? undefined : getReqUrl(setu.urls.regular);
+
+      // 反和谐
+      const base64 =
+        isGroupMsg &&
+        setting.antiShielding &&
+        (await getAntiShieldingBase64(url, fallbackUrl).catch(e => {
+          console.error(`${global.getTime()} [error] anti shielding`);
+          console.error(url);
+          console.error(e);
+          if (String(e).includes('Could not find MIME for Buffer') || String(e).includes('status code 404')) {
+            return PIXIV_404;
+          }
+          global.replyMsg(context, 'Modifying Error');
+        }));
+
+      if (base64 === PIXIV_404) {
+        global.replyMsg(context, 'Sending Error');
+        return;
+      }
+
+      success = true;
+    })
+    .catch(e => {
+      console.error(`${global.getTime()} [error]`);
+      console.error(e);
+      global.replyMsg(context, 'Server Error', at);
+    })
+    .finally(() => {
+      if (!success) console.log(context.user_id);
+    });
+
+  return true;
+}
+
+export default sendImg;
+
+function getSetuUrlByTemplate(tpl, setu, url) {
+  const path = new URL(url).pathname.replace(/^\//, '');
+  if (!/{{.+}}/.test(tpl)) return new URL(path, tpl).href;
+  return _.template(tpl, { interpolate: /{{([\s\S]+?)}}/g })({ path, ..._.pick(setu, ['pid', 'p', 'uid', 'ext']) });
+}
